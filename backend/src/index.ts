727751e42ccadf981883fe9f448fde0b3bc04aa3
Karan <karansharma192004@gmail.com>
2025-05-15 19:39:06 +0530

    D1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,11 +10,12 @@ import cookieParser from 'cookie-parser';
 require('dotenv').config();
 
 const app = express();
-
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
-}));
+  }));
+  
+
 app.use(cookieParser());
 app.use(express.json());
 
@@ -65,8 +66,9 @@ app.post('/signin', async (req, res) => {
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: true, // true in production
+        secure: process.env.NODE_ENV === 'production',
     });
+
     
     res.json({
         message: "Signin success",
